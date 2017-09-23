5a68e02dcc9afb283acedc52bf2fcdcb56edf1de
jiangchao0304 <14317048@qq.com>
2017-09-23 17:32:07 +0800

    结构调整

---
+++ erp-product/src/main/java/com/sunvalley/erp/product/controller/HomeController.java
@@ -0,0 +1,14 @@
+/*
+* Copyright (c) 2017 sunvalley. All Rights Reserved.
+*/
+package com.sunvalley.erp.product.controller;
+
+/**
+ * 类或方法的功能描述 :TODO
+ *
+ * @Author: douglas.jiang
+ * @Date: 2017-09-23 17:31
+ */
+public class HomeController {
+}
+
