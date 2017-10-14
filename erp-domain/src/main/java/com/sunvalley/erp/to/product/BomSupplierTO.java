17058c31cebdf8e4753f4cab1c58e553fc0d9ea7
jiangchao0304 <14317048@qq.com>
2017-10-14 13:23:29 +0800

    ERP-19287 (bomSupplierTO GET SET)

--- erp-domain/src/main/java/com/sunvalley/erp/to/product/BomSupplierTO.java
+++ erp-domain/src/main/java/com/sunvalley/erp/to/product/BomSupplierTO.java
@@ -20,6 +20,52 @@ public class BomSupplierTO implements Serializable{
     private String purchaserName;
     private String purPropertyStr;
 
+    public Integer getSupplierid() {
+        return supplierid;
+    }
 
+    public void setSupplierid(Integer supplierid) {
+        this.supplierid = supplierid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPurProperty() {
+        return purProperty;
+    }
+
+    public void setPurProperty(Integer purProperty) {
+        this.purProperty = purProperty;
+    }
+
+    public int getPurchaserid() {
+        return purchaserid;
+    }
+
+    public void setPurchaserid(int purchaserid) {
+        this.purchaserid = purchaserid;
+    }
+
+    public String getPurchaserName() {
+        return purchaserName;
+    }
+
+    public void setPurchaserName(String purchaserName) {
+        this.purchaserName = purchaserName;
+    }
+
+    public String getPurPropertyStr() {
+        return purPropertyStr;
+    }
+
+    public void setPurPropertyStr(String purPropertyStr) {
+        this.purPropertyStr = purPropertyStr;
+    }
 }
 
